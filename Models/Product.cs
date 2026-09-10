534619832489d993efd436fb42eec437a3a5beb9
Shigosag (Segun Arulogun Gabriel) <88502810+Shigosag@users.noreply.github.com>
2026-09-10 16:51:34 +0000

    Update Product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,6 @@
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShigosagNexusERP.Models;
 
@@ -6,10 +8,24 @@ public class Product
 {
     [Key]
     public int Id { get; set; }
+
     [Required]
+    [MaxLength(120)]
     public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(50)]
     public string SKU { get; set; } = string.Empty;
+
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
+
     public int StockLevel { get; set; }
+
+    [MaxLength(60)]
     public string Category { get; set; } = "General";
+
+    public bool IsArchived { get; set; } = false;
+
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
